fe431cb0aaeccb2ddedc9145e02f5ba2e4fd9427
Eduardo Chiarotti <dudumelgaco@hotmail.com>
2025-09-25 20:32:28 -0300

    feat: fix issue import

--- src/research_agents/crew.py
+++ src/research_agents/crew.py
@@ -2,7 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-from crewai.tools import SerperDevTool
+from crewai_tools import SerperDevTool
 
 print("test")
 print("test")
